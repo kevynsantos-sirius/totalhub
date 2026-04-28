40d17a88cc654a890224b26d28a2b67544fadfe7
KevynSantos <52923238+KevynSantos@users.noreply.github.com>
2026-04-27 22:59:48 -0300

    FEAT: Melhorando botao de solicitar demonstracao

--- src/components/CTAFinal.jsx
+++ src/components/CTAFinal.jsx
@@ -1,13 +1,19 @@
 export default function CTAFinal() {
   return (
-    <section className="section cta">
+    <section className="section">
       <h2>Transforme sua operação com inteligência</h2>
 
       <p>
         Transforme dados em insights, insights em jornadas e jornadas em resultados.
       </p>
 
-      <button>Solicitar demonstração</button>
+      <button
+        className="cta-button-primary"
+        onClick={() => alert("Abrir fluxo de demonstração 🚀")}
+      >
+        <span>Solicitar demonstração</span>
+        <span className="arrow">→</span>
+      </button>
     </section>
   );
 }
