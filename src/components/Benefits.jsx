050acd8a7a8d170d180e70158bd2f78459b22425
KevynSantos <52923238+KevynSantos@users.noreply.github.com>
2026-05-05 10:09:25 -0300

    https://sirius-team-ch6cxuf1.atlassian.net/browse/TT-1?atlOrigin=eyJpIjoiNzVhZTQwNzAzMWQyNGM1ODkwYmI0MGU3NjUyMzczYjkiLCJwIjoiaiJ9
    Ajuste de layout

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -5,17 +5,17 @@ export default function Benefits() {
 
       <div className="results-grid">
         <div className="result-card time">
-          <span className="highlight">+Eficiência</span>
+          <span className="highlight">+ Eficiência</span>
           <p>Automação completa das jornadas</p>
         </div>
 
         <div className="result-card error">
-          <span className="highlight">-Custos</span>
+          <span className="highlight">- Custos</span>
           <p>Redução de retrabalho e desperdício</p>
         </div>
 
         <div className="result-card speed">
-          <span className="highlight">+Velocidade</span>
+          <span className="highlight">+ Velocidade</span>
           <p>Decisões baseadas em dados</p>
         </div>
       </div>
