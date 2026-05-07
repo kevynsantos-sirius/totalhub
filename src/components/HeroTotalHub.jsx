0ae51232ef8d153d890f46b173ee1ff39451fd85
KevynSantos <52923238+KevynSantos@users.noreply.github.com>
2026-05-07 11:09:38 -0300

    Adicionando imagem de fundo

--- src/components/HeroTotalHub.jsx
+++ src/components/HeroTotalHub.jsx
@@ -1,9 +1,13 @@
-export default function HeroTotalHub() {
-  return (
-    <section className="hero">
-      <div className="glow"></div>
+import { useState } from "react";
+import image from '../assets/background.png';
 
-      <div>
+export default function HeroTotalHub() {
+
+  return (
+    <section className="hero" style={{ backgroundImage: `url(${image})` }}>
+      <div className="overlay"></div>
+
+      <div className="hero-text">
         <h1>
           Sua operação está espalhada em várias ferramentas{" "}
           <text>que não se conversam?</text>
@@ -11,8 +15,8 @@ export default function HeroTotalHub() {
 
         <p>
           Empresas continuam operando com comunicação fragmentada,
-          processos manuais e pouca visibilidade.
-          Isso gera custos, atrasos e perda de eficiência.
+          processos manuais e pouca visibilidade. Isso gera custos, atrasos e
+          perda de eficiência.
         </p>
 
         <p className="subtitle">
