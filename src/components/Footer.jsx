0e4da1f4e98892bca158c994205deea07cbcbdec
KevynSantos <52923238+KevynSantos@users.noreply.github.com>
2026-05-07 17:18:22 -0300

    Aplicando rodapé simples com logo, frase dos direitos e redes sociais

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,17 @@
 export default function Footer() {
   return (
     <footer className="footer">
-      <h3>TotalHub</h3>
-
-      <p>
-        Plataforma modular de comunicação, automação e integração.
-      </p>
-
-      <p style={{ marginTop: "20px", fontSize: "14px" }}>
-        © {new Date().getFullYear()} TotalHub. Todos os direitos reservados.
-      </p>
-    </footer>
+    <div className="footer-container">
+      <img src="/src/assets/logo_totalhub_vetor_color_Branco.png" alt="TotalHub Logo" className="footer-logo" />
+      <div className="footer-rights">
+        <p>&copy; 2026 TotalHub. Todos os direitos reservados.</p>
+      </div>
+      <div className="footer-social">
+        <a href="https://www.linkedin.com/company/TotalHub" target="_blank" rel="noopener noreferrer">
+          <img src="/src/assets/linkedin.png" alt="LinkedIn" className="social-icon" />
+        </a>
+      </div>
+    </div>
+  </footer>
   );
 }
