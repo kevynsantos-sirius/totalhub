f48e7b4a2610df31cd1640c1d145b526058a5369
KevynSantos <52923238+KevynSantos@users.noreply.github.com>
2026-05-07 17:34:13 -0300

    Links do footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,6 +5,12 @@ export default function Footer() {
       <img src="/src/assets/logo_totalhub_vetor_color_Branco.png" alt="TotalHub Logo" className="footer-logo" />
       <div className="footer-rights">
         <p>&copy; 2026 TotalHub. Todos os direitos reservados.</p>
+          <div style={{ marginTop: "20px" }}>
+            <a href="#">TotalDocs</a> |{" "}
+            <a href="#">TotalSign</a> |{" "}
+            <a href="#">TotalBot</a> |{" "}
+            <a href="#">TotalSync</a>
+        </div>
       </div>
       <div className="footer-social">
         <a href="https://www.linkedin.com/company/TotalHub" target="_blank" rel="noopener noreferrer">
